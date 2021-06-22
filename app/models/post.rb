f190da204fa4f9e1fcab90efb57c99f7fe9f88af
TimLit13 <litidm@gmail.com>
2021-06-22 18:57:16 +0300

    Add routes for tags and tags controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,15 @@ class Post < ApplicationRecord
   
   validates :title, :summary, :body, :image, presence: true
   validates :title, :summary, :body, length: { minimum: 3} 
+
+  def all_tags
+    self.tags.map(&:name).join(', ')
+  end
+
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
 end
