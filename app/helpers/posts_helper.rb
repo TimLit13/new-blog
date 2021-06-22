ac35e2d21f0ae6d0c4c7879bbc1569fc2661dc7f
TimLit13 <litidm@gmail.com>
2021-06-22 20:06:01 +0300

    Add tags cloud

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+  def tag_cloud(tags, classes)
+    max = tags.sort_by(&:count).last
+    maximum = max.count.to_f
+
+    tags.each do |tag|
+      index = (tag.count.to_f / maximum) * (classes.length - 1)
+      yield(tag, classes[index.round])
+    end
+  end
 end
