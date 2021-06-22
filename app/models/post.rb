ff9426176bd5e6d01989582c7fdb6d6c0a7e9d3e
TimLit13 <litidm@gmail.com>
2021-06-22 18:13:00 +0300

    Add links between post tag and tagging models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
 
+  has_many :taggings
+  has_many :tags, through: :taggings
+  
   validates :title, :summary, :body, :image, presence: true
   validates :title, :summary, :body, length: { minimum: 3} 
 end
