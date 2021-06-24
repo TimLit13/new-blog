5f524dbc0f2a74e126a6be4eea4e55da2e4146a0
TimLit13 <litidm@gmail.com>
2021-06-24 21:21:54 +0300

    Add parent category by ancestry gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_133510) do
+ActiveRecord::Schema.define(version: 2021_06_24_181021) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "pictures", force: :cascade do |t|
