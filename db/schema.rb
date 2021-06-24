f1c39b2a8ca48d9cfd1c9b176ba05a52023345e3
TimLit13 <litidm@gmail.com>
2021-06-24 17:22:50 +0300

    Add migration add category_id to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_110454) do
+ActiveRecord::Schema.define(version: 2021_06_24_133510) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_06_24_110454) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
   create_table "taggings", force: :cascade do |t|
