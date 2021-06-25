1d88b40adbdc240b8b444cf314617fc49cd85d89
TimLit13 <litidm@gmail.com>
2021-06-25 18:15:18 +0300

    Authenticate before actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-
+  before_action :authenticate_user!, except: [:show]
   before_action :set_category, only: %i[show edit update destroy]
   before_action :set_categories, only: %i[index]
 
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,4 @@
 class Picture < ApplicationRecord
+  before_action :authenticate_user!
   mount_uploader :image, ImageUploader
 end
