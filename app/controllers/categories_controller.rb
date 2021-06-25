db23006a1019e3ab0731e09a7a77ffed2f85b937
TimLit13 <litidm@gmail.com>
2021-06-25 15:47:46 +0300

    Fix bug with creating instead of updating

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,7 +14,7 @@ class CategoriesController < ApplicationController
 #=======================================================================================
   def new
     @category = Category.new
-
+    # @categories = Category.all.order(:name)
   end
 
   def create
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,13 @@
 module PostsHelper
   def tag_cloud(tags, classes)
-    max = tags.sort_by(&:count).last
-    maximum = max.count.to_f
+    if tags && classes
+      max = tags.sort_by(&:count).last
+      maximum = max.count.to_f
 
-    tags.each do |tag|
-      index = (tag.count.to_f / maximum) * (classes.length - 1)
-      yield(tag, classes[index.round])
+      tags.each do |tag|
+        index = (tag.count.to_f / maximum) * (classes.length - 1)
+        yield(tag, classes[index.round])
+      end
     end
   end
 end
