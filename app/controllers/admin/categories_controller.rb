0843a7c1d9fdd90ff39a6e0e002e04ff599660b9
TimLit13 <litidm@gmail.com>
2021-06-27 08:33:13 +0300

    Replace views for admin

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,70 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  # , except: [:show]
+  before_action :set_category, only: %i[edit update destroy]
+  before_action :set_categories, only: %i[index]
+
+#=======================================================================================
+  def index
+    
+  end
+ 
+  # def show
+  #   @posts = Post.where(category_id: [@category.subtree_ids]).paginate(page: params[:page], per_page: 5).order(updated_at: :desc)
+  # end
+#=======================================================================================
+  def new
+    @category = Category.new
+    # @categories = Category.all.order(:name)
+  end
+
+  def create
+    @category = Category.new (category_params)
+
+    if @category.valid?
+      @category.save
+      redirect_to categories_path, success: "#{t('categories.create.success')}"
+    else
+      flash.now[:danger] = "#{t('categories.create.fault')}"
+      render :new
+    end
+  end
+#=======================================================================================
+  def edit
+
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, success: "#{t('categories.update.success')}"
+    else
+      flash.now[:danger] = "#{t('categories.update.fault')}"
+      render :edit
+    end
+
+  end
+#=======================================================================================
+  def destroy
+    if @category.destroy
+      redirect_to categories_path, info: "#{t('categories.delete.success')}"
+    else 
+      render :edit, danger: "#{t('categories.delete.fault')}"
+    end
+  end
+#=======================================================================================
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+  
+  def set_categories
+    @categories = Category.all.order(updated_at: :desc)
+ 
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :id, :parent_id)
+  end
+end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,56 +1,57 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
-  before_action :set_category, only: %i[show edit update destroy]
-  before_action :set_categories, only: %i[index]
+  # before_action :authenticate_user!, except: [:show]
+  before_action :set_category, only: %i[show] 
+  # edit update destroy]
+  # before_action :set_categories, only: %i[index]
 
-#=======================================================================================
-  def index
+# #=======================================================================================
+#   def index
     
-  end
+#   end
  
   def show
     @posts = Post.where(category_id: [@category.subtree_ids]).paginate(page: params[:page], per_page: 5).order(updated_at: :desc)
   end
-#=======================================================================================
-  def new
-    @category = Category.new
-    # @categories = Category.all.order(:name)
-  end
+# #=======================================================================================
+#   def new
+#     @category = Category.new
+#     # @categories = Category.all.order(:name)
+#   end
 
-  def create
-    @category = Category.new (category_params)
+#   def create
+#     @category = Category.new (category_params)
 
-    if @category.valid?
-      @category.save
-      redirect_to categories_path, success: "#{t('categories.create.success')}"
-    else
-      flash.now[:danger] = "#{t('categories.create.fault')}"
-      render :new
-    end
-  end
-#=======================================================================================
-  def edit
+#     if @category.valid?
+#       @category.save
+#       redirect_to categories_path, success: "#{t('categories.create.success')}"
+#     else
+#       flash.now[:danger] = "#{t('categories.create.fault')}"
+#       render :new
+#     end
+#   end
+# #=======================================================================================
+#   def edit
 
-  end
+#   end
 
-  def update
-    if @category.update(category_params)
-      redirect_to categories_path, success: "#{t('categories.update.success')}"
-    else
-      flash.now[:danger] = "#{t('categories.update.fault')}"
-      render :edit
-    end
+#   def update
+#     if @category.update(category_params)
+#       redirect_to categories_path, success: "#{t('categories.update.success')}"
+#     else
+#       flash.now[:danger] = "#{t('categories.update.fault')}"
+#       render :edit
+#     end
 
-  end
-#=======================================================================================
-  def destroy
-    if @category.destroy
-      redirect_to categories_path, info: "#{t('categories.delete.success')}"
-    else 
-      render :edit, danger: "#{t('categories.delete.fault')}"
-    end
-  end
-#=======================================================================================
+#   end
+# #=======================================================================================
+#   def destroy
+#     if @category.destroy
+#       redirect_to categories_path, info: "#{t('categories.delete.success')}"
+#     else 
+#       render :edit, danger: "#{t('categories.delete.fault')}"
+#     end
+#   end
+# #=======================================================================================
 
   private
 
@@ -58,10 +59,10 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
   
-  def set_categories
-    @categories = Category.all.order(updated_at: :desc)
+  # def set_categories
+  #   @categories = Category.all.order(updated_at: :desc)
  
-  end
+  # end
 
   def category_params
     params.require(:category).permit(:name, :id, :parent_id)
