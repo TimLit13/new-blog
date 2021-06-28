ce6b1cc676dfa4b2dbd8078432c744451d6bd7fa
TimLit13 <litidm@gmail.com>
2021-06-28 16:18:41 +0300

    Fix routes for contacts and terms

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/terms'
-  get 'static_pages/contacts'
+  
   # scope "(:locale)", locale: /ru|en/ do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users
     root to: 'posts#index'
+
+    get 'terms', to: 'static_pages#terms', as: 'terms'
+    get 'contacts', to: 'static_pages#contacts',  as: 'contacts'
 
     resources :posts, only: [:show, :index]
     resources :pictures, only: [:create, :destroy]
