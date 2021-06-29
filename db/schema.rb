b20b1a16e255f4062660fed8cadb58b667fbcd8e
TimLit13 <litidm@gmail.com>
2021-06-29 15:33:25 +0300

    Configure sqlite for test env

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_06_27_051953) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -41,8 +38,8 @@ ActiveRecord::Schema.define(version: 2021_06_27_051953) do
   end
 
   create_table "taggings", force: :cascade do |t|
-    t.bigint "post_id", null: false
-    t.bigint "tag_id", null: false
+    t.integer "post_id", null: false
+    t.integer "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_taggings_on_post_id"
