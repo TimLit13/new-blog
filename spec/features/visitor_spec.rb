a00429ba67b1c76e7d24d69edf00a0dcb4bd2356
TimLit13 <litidm@gmail.com>
2021-07-02 09:30:24 +0300

    Add specs for static pages

--- spec/features/visitor_spec.rb
+++ spec/features/visitor_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
+
+feature 'Visitor go to home page' do
+  scenario 'Allows visitor watch all articles' do
+    visit '/'
+
+    expect(page).to have_content I18n.t('common.articles')  
+  end
+end
+
+feature 'Visitor go to terms page' do
+  scenario 'Allows visitor watch terms of app' do
+    visit '/terms'
+
+    expect(page).to have_content I18n.t('static_pages.terms.terms')  
+  end
+end
+
+feature 'Visitor go to contacts page' do
+  scenario 'Allows visitor watch contacts' do
+    visit '/contacts'
+
+    expect(page).to have_content I18n.t('static_pages.contacts.contacts')  
+  end
+end
+
 feature 'Visitor sign in' do
   scenario 'Allows visitor access to authentication page' do
     visit '/users/sign_in'
